2d14144c096e7027f53102abd3886667aeeac366
Kavitha2642 <147233839+Kavitha2642@users.noreply.github.com>
2023-10-28 20:57:44 +0530

    Delete portifolio directory

--- portifolio/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-
-// Function to alert a message when the "Resume" link is clicked
-document.getElementById("resume-link").addEventListener("click", function() {
-    alert("You clicked the Resume link!");
-    // You can replace the alert with any desired action like tracking the click event or opening the PDF file.
-});
